598e8cab8a8616e96780ece63660ea8066d0ceb4
jfoyarzo <foyarzo621@gmail.com>
2023-05-11 22:32:21 -0400

    Update routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root "tasks#index"
+  post "tasks/:id/toggle", to: "tasks#toggle"
   resources :tasks
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
 end
