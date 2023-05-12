eff7678dd5a4ea7246a9c7275d05b69a4db8e4cc
jfoyarzo <foyarzo621@gmail.com>
2023-05-11 22:02:39 -0400

    Update task controller with create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,24 @@
 class TasksController < ApplicationController
-    def index
-        @tasks = Task.all
+  def index
+    @tasks = Task.all
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to tasks_url, notice: 'Task created successfully' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
     end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
+  end
 end
