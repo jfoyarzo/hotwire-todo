7a53f9e33850810fab03b4ca9dbd66db8e2ca0d4
jfoyarzo <foyarzo621@gmail.com>
2023-05-12 20:25:17 -0400

    Add edit, update and destroy actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,9 +23,30 @@ class TasksController < ApplicationController
     render json: { message: "Success" }
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+   def update
+    @task = Task.find(params[:id])
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to tasks_url, notice: "Task was successfully updated" }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url, notice: "Post was successfully deleted."
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:description)
+    params.require(:task).permit(:description, :id)
   end
 end
